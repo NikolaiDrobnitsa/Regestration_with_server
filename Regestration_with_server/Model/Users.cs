2d2884117a38658adff79b117af7290010b10c21
Микола Дробница <nikolaidrobnitsa@gmail.com>
2022-08-21 23:30:05 +0300

    add model

---
+++ Regestration_with_server/Model/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regestration_with_server.Model
+{
+    internal class Users
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password_hesh { get; set; }
+        public override string ToString()
+        {
+            return string.Format("id: {0}\tlogin: {1}\tpassword_hesh: {2}", id, login, password_hesh);
+        }
+        public Users()
+        {
+
+        }
+    }
+}
